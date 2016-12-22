36079d92017154cec11c0038eac36e297e2c5068
isen <3104255643@qq.com>
2016-12-22 10:13:34 +0800

    framework

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Expres' });
+});
+
+module.exports = router;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+var nodeExternals = require('webpack-node-externals');
+
+var publicPath = 'http://localhost:3000/';
+/**代表开启热加载模式  ？后的参数代表，不能热加载的情况就整页刷新 */
+var hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
+
+
+var devConfig = {
+    entry: {
+        page1: [path.resolve(__dirname, './recourse/page1'),hotMiddlewareScript],
+    },
+    output: {
+        filename: './[name]/bundle.js',
+        path: path.resolve(__dirname, './public'),
+        publicPath: publicPath
+    },
+    devtool: 'source-map',
+    module: {
+            loaders: [{
+                test: /\.(png|jpg)$/,
+                loader: 'url?limit=8192&context=client&name=[path][name].[ext]'
+            }, {
+                test: /\.css$/,
+                loader: "style!css"
+            },{
+                test: /\.js|jsx$/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },{
+                test: require.resolve('jquery'),
+                loader: 'expose?jQuery!expose?$'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+};
+
+module.exports = devConfig;
